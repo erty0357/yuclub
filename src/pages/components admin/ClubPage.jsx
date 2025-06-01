858992f20f8fe9bfa6c1d8f9d3c794d80d6bed90
erty0357 <erty0357@naver.com>
2025-06-02 02:47:46 +0900

    Update ClubPage.jsx

--- src/pages/components admin/ClubPage.jsx
+++ src/pages/components admin/ClubPage.jsx
@@ -3,7 +3,6 @@ import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
 import { useNavigate } from 'react-router-dom';
 import NoticeListPage from "../components member/NoticeListPage";
-console.log("✅ members 값:", members, "타입:", typeof members);
 
 export default function ClubPage({
   clubName,
