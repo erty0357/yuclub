76a3d63cc8179d6968b2a393cf1977ca4afe2175
erty0357 <erty0357@naver.com>
2025-06-02 03:35:51 +0900

    Update memberpage.jsx

--- src/pages/memberpage.jsx
+++ src/pages/memberpage.jsx
@@ -17,7 +17,7 @@ const ClubMemberPage = () => {
 
   useEffect(() => {
   const role = localStorage.getItem('userRole');
-  axios.get(`/api/clubs/${encodeURIComponent(clubName)}?role=${role}`)
+  axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/clubs/${encodeURIComponent(clubName)}/members/status`, {
     .then(res => {
       setClubData(res.data);
     })
