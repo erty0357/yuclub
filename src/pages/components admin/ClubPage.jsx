21b00f9d64ddc02d9acd46e6468a1bb177129a79
erty0357 <erty0357@naver.com>
2025-06-02 18:39:08 +0900

    Update ClubPage.jsx

--- src/pages/components admin/ClubPage.jsx
+++ src/pages/components admin/ClubPage.jsx
@@ -152,7 +152,7 @@ export default function ClubPage({
           </div>
 
           <div style={{ backgroundColor: '#eef1ec', padding: '1rem', marginTop: '1rem', borderRadius: '8px' }}>
-            <NoticeListPage notices={notices} />
+            <NoticeListPage />
           </div>
 
           <div style={{ backgroundColor: '#fffbe6', padding: '1rem', marginTop: '1rem', borderRadius: '8px', border: '1px dashed #facc15' }}>
